6e003e650515c55fb2b30db57a7fa6d930fca984
Ehren Abbott <era12358@gmail.com>
2024-05-15 12:44:21 -0500

    Imported signInWithPopup and created authSignInWithGoogle

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import {
     signInWithEmailAndPassword, 
     signOut, 
     onAuthStateChanged, 
-    GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
+    GoogleAuthProvider, 
+    signInWithPopup } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"
 
 const firebaseConfig = {
     apiKey: "AIzaSyAfqNeWbyEXrH-RcucrykVdk3NSFIZuZ8M",
@@ -61,8 +62,35 @@ onAuthStateChanged(auth, (user) => {
     }
 })
 
+// function authSignInWithGoogle() {
+//     signInWithPopup(auth, provider)
+//         .then((result) => {
+//             // This gives you a Google Access Token. You can use it to access the Google API.
+//             const credential = GoogleAuthProvider.credentialFromResult(result);
+//             const token = credential.accessToken;
+//             // The signed-in user info.
+//             const user = result.user;
+//             console.log("Google sign in successful")
+//             // IdP data available using getAdditionalUserInfo(result)
+//         }).catch((error) => {
+//             // Handle Errors here.
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             // The email of the user's account used.
+//             const email = error.customData.email;
+//             // The AuthCredential type that was used.
+//             const credential = GoogleAuthProvider.credentialFromError(error);
+//             console.error("Error with google sign in")
+//         });
+// }
+
 function authSignInWithGoogle() {
-    console.log("Sign in with Google")
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log("Signed in with Google")
+        }).catch((error) => {
+            console.error(error.message)
+        })
 }
 
 function authSignInWithEmail() {
