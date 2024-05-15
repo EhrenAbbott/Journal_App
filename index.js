8c0de27f0c28b1d79316c895b989e9853145fbda
Ehren Abbott <era12358@gmail.com>
2024-05-15 15:00:50 -0500

    Created showProfilePicture to display placeholder image if no custom image  URL exists

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ const createAccountButtonEl = document.getElementById("create-account-btn")
 
 const signOutButtonEl = document.getElementById("sign-out-btn")
 
+const userProfilePictureEl = document.getElementById("user-profile-picture")
+
 
 /* == UI - Event Listeners == */
 
@@ -57,32 +59,12 @@ showLoggedOutView()
 onAuthStateChanged(auth, (user) => {
     if (user) {
         showLoggedInView()
+        showProfilePicture(userProfilePictureEl, user)
     } else {
         showLoggedOutView()
     }
 })
 
-// function authSignInWithGoogle() {
-//     signInWithPopup(auth, provider)
-//         .then((result) => {
-//             // This gives you a Google Access Token. You can use it to access the Google API.
-//             const credential = GoogleAuthProvider.credentialFromResult(result);
-//             const token = credential.accessToken;
-//             // The signed-in user info.
-//             const user = result.user;
-//             console.log("Google sign in successful")
-//             // IdP data available using getAdditionalUserInfo(result)
-//         }).catch((error) => {
-//             // Handle Errors here.
-//             const errorCode = error.code;
-//             const errorMessage = error.message;
-//             // The email of the user's account used.
-//             const email = error.customData.email;
-//             // The AuthCredential type that was used.
-//             const credential = GoogleAuthProvider.credentialFromError(error);
-//             console.error("Error with google sign in")
-//         });
-// }
 
 function authSignInWithGoogle() {
     signInWithPopup(auth, provider)
@@ -158,4 +140,12 @@ function clearInputField(field) {
 function clearAuthFields() {
 	clearInputField(emailInputEl)
 	clearInputField(passwordInputEl)
+}
+
+function showProfilePicture(imgElement, user){ 
+    if (user.photoURL){ 
+        imgElement.src = user.photoURL
+    } else { 
+        imgElement.src = "assets/icons/profile.icon.webp"
+    }
 }
